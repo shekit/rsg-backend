7de3e174b11ac8445ddf4474f34a73e4b3365263
shekit <as7790@nyu.edu>
2017-11-14 00:53:57 -0500

    added server response

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var http = require('http');
-var server = http.createServer()
+var server = http.createServer(function(req,res){
+  res.writeHead(200, {'Content-Type':'text/plain'});
+  res.end("okay");
+})
 var io = require('socket.io')({transports:['websocket']});
 
 io.attach(4567);
