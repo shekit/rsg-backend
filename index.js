06f35b1ccd5d89350488fead128c16bd847456c5
shekit <as7790@nyu.edu>
2017-11-18 17:56:02 -0500

    changed reconnect time

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ io.on('connection', function(socket){
   function disconnectClient(){
     timer = setTimeout(function(){
       socket.disconnect()
-    },12000)
+    },15000)
   }
 
   socket.on('results', function(){
